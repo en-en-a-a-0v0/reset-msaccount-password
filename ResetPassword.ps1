049e513b3bb9ffa7d3d9101a1ea6bc1f88cd22c8
en-en-a-a-0v0 <45622952+en-en-a-a-0v0@users.noreply.github.com>
2021-11-23 17:32:11 +1100

    Update ResetPassword.ps1

--- ResetPassword.ps1
+++ ResetPassword.ps1
@@ -1,28 +1,35 @@
-﻿# Main Function to Update Password
-Function Update_Password([string]$mgrusername,[string]$mgrpassword,[string]$username,[string]$password){
-    
-    
+# Main Function to Update Password
+Function Show_Notification([string]$s){
+    [System.Windows.Forms.MessageBox]::Show($s, "Notification") 
+}
+
+Function Show_Error([string]$e){
+    [System.Windows.Forms.MessageBox]::Show($e, "Error") 
+}
+
+Function Check_Plugin(){
     if (-Not (Get-Module -ListAvailable -Name MSOnline)) {
-        
-        
-        $LabelMsg.Text = "Installing module... If any notification comes out, please click ""Yes to All"""
-        $NotificationForm.Hide()
-        $NotificationForm.Show()
-        install-module MSOnline
+        Show_Notification "Installing module... If any notification comes out, please click ""Yes to All"""       
+        try{
+            install-module MSOnline
+        }
+        catch{
+             Show_Error "Error!!! Unable to install plugin: MSOnline. Please install manually."
+        }
     }
-    $LabelMsg.Text = "Initiallizing Module"
-    $NotificationForm.Hide()
-        $NotificationForm.Show()
+}
+Function Update_Password([string]$mgrusername,[string]$mgrpassword,[string]$username,[string]$password){    
+    Check_Plugin
     import-module MSOnline
     $PWord = ConvertTo-SecureString -String $mgrpassword -AsPlainText -Force
     $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $mgrusername, $PWord
     $LabelMsg.Text = "Getting Credential"
     $NotificationForm.Hide()
-        $NotificationForm.Show()
+    $NotificationForm.Show()
     Connect-MsolService -Credential $Credential
     $LabelMsg.Text = "Changing Password"
     $NotificationForm.Hide()
-        $NotificationForm.Show()
+    $NotificationForm.Show()
     Set-MsolUserPassword -UserPrincipalName $username -NewPassword $password -ForceChangePassword:$False
     $LabelMsg.Text = "Success"
     $NotificationForm.Hide()
@@ -31,64 +38,82 @@ Function Update_Password([string]$mgrusername,[string]$mgrpassword,[string]$user
 
 Add-Type -AssemblyName System.Windows.Forms
 $PowerShellForms=New-Object System.Windows.Forms.Form
-$PowerShellForms.Text = "Reset Password"
+$PowerShellForms.Text = "VIA Microsoft Account Managment Tool"
 $PowerShellForms.Size = New-Object System.Drawing.Size(300,450) 
 $PowerShellForms.StartPosition = "CenterScreen"
 $PowerShellForms.SizeGripStyle = "Hide"
+
+$FormTabControl = New-object System.Windows.Forms.TabControl 
+$FormTabControl.Size = "755,475" 
+$PowerShellForms.Controls.Add($FormTabControl)
+
+$Tab1 = New-object System.Windows.Forms.Tabpage
+$Tab1.DataBindings.DefaultDataSourceUpdateMode = 0 
+$Tab1.UseVisualStyleBackColor = $True 
+$Tab1.Name = "Tab1" 
+$Tab1.Text = "Reset Passwords” 
+$FormTabControl.Controls.Add($Tab1)
+
+$Tab2 = New-object System.Windows.Forms.Tabpage
+$Tab2.DataBindings.DefaultDataSourceUpdateMode = 0 
+$Tab2.UseVisualStyleBackColor = $True 
+$Tab2.Name = "Tab1" 
+$Tab2.Text = "Create accounts” 
+$FormTabControl.Controls.Add($Tab2)
 
 # Username Text
 $LabelMgrUserName = New-Object System.Windows.Forms.Label
 $LabelMgrUserName.Location = New-Object System.Drawing.Point(10,20) 
 $LabelMgrUserName.Text = "MgrUserName:"
 $LabelMgrUserName.AutoSize = $True
-$PowerShellForms.Controls.Add($LabelMgrUserName)
+$Tab1.Controls.Add($LabelMgrUserName)
 
 # Username TextBox
 $TextBoxMgrUserName = New-Object System.Windows.Forms.TextBox
 $TextBoxMgrUserName.Text="itdeveloper@online.via.edu.au"
 $TextBoxMgrUserName.Location = New-Object System.Drawing.Point(10,50)
 $TextBoxMgrUserName.Size = New-Object System.Drawing.Size(260,20)
-$PowerShellForms.Controls.Add($TextBoxMgrUserName)
+$Tab1.Controls.Add($TextBoxMgrUserName)
 
 # Password Text
 $LabelMgrPassWord = New-Object System.Windows.Forms.Label
 $LabelMgrPassWord.Location = New-Object System.Drawing.Point(10,90) 
 $LabelMgrPassWord.Text = "MgrPassWord:"
 $LabelMgrPassWord.AutoSize = $True
-$PowerShellForms.Controls.Add($LabelMgrPassWord)
+$Tab1.Controls.Add($LabelMgrPassWord)
 
 # Password TextBox
 $TextBoxMgrPassWord = New-Object System.Windows.Forms.TextBox
 $TextBoxMgrPassWord.Location = New-Object System.Drawing.Point(10,120)
 $TextBoxMgrPassWord.Size = New-Object System.Drawing.Size(260,20)
-$PowerShellForms.Controls.Add($TextBoxMgrPassWord)
+$Tab1.Controls.Add($TextBoxMgrPassWord)
 
 # Username Text
 $LabelUserName = New-Object System.Windows.Forms.Label
 $LabelUserName.Location = New-Object System.Drawing.Point(10,160) 
 $LabelUserName.Text = "UserName:"
 $LabelUserName.AutoSize = $True
-$PowerShellForms.Controls.Add($LabelUserName)
+$Tab1.Controls.Add($LabelUserName)
 
 # Username TextBox
 $TextBoxUserName = New-Object System.Windows.Forms.TextBox
 $TextBoxUserName.Location = New-Object System.Drawing.Point(10,190)
 $TextBoxUserName.Text="test_365login@online.via.edu.au"
 $TextBoxUserName.Size = New-Object System.Drawing.Size(260,20)
-$PowerShellForms.Controls.Add($TextBoxUserName)
+$Tab1.Controls.Add($TextBoxUserName)
 
 # Password Text
 $LabelPassWord = New-Object System.Windows.Forms.Label
 $LabelPassWord.Location = New-Object System.Drawing.Point(10,230) 
 $LabelPassWord.Text = "PassWord:"
 $LabelPassWord.AutoSize = $True
-$PowerShellForms.Controls.Add($LabelPassWord)
+$Tab1.Controls.Add($LabelPassWord)
 
 # Password TextBox
 $TextBoxPassWord = New-Object System.Windows.Forms.TextBox
 $TextBoxPassWord.Location = New-Object System.Drawing.Point(10,260)
 $TextBoxPassWord.Size = New-Object System.Drawing.Size(260,20)
-$PowerShellForms.Controls.Add($TextBoxPassWord)
+$Tab1.Controls.Add($TextBoxPassWord)
 
 # Nofitication Form
 $NotificationForm=New-Object System.Windows.Forms.Form
@@ -110,7 +135,7 @@ $SubmitButton.Add_Click({
     Update_Password $TextBoxMgrUserName.Text $TextBoxMgrPassWord.Text $TextBoxUserName.Text $TextBoxPassWord.Text
     
 })
-$PowerShellForms.Controls.Add($SubmitButton)
+$Tab1.Controls.Add($SubmitButton)
 
 # Test Button
 $TestForms=New-Object System.Windows.Forms.Form
@@ -144,7 +169,7 @@ $Test.Add_Click({
     $TestForms.Show()
     
 })
-$PowerShellForms.Controls.Add($Test)
+$Tab1.Controls.Add($Test)
 # Show
 $PowerShellForms.ShowDialog()
 
